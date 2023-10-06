01bcc4d36556d37380367ac5c026eeadfa251636
headshot3833 <117528008+headshot3833@users.noreply.github.com>
2023-10-06 17:37:04 +0400

    Добавьте файлы проекта.

---
+++ Math/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace mathTRiangle
+{
+    public interface ICircle
+    {
+        double CalculateArea(double radius);
+    }
+    public interface iTriangle
+    {
+        double CalculateArea(double a, double b, double c);
+    }
+    public class Circle : ICircle
+    {
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+    }
+    public class Triangle : iTriangle
+    {
+        public double CalculateArea(double a, double b, double c) 
+        {
+            double S = (a + b + c)/2;
+
+            return Math.Sqrt(S * (S - a) * (S - b) * (S - c));
+        }
+    }
+}
